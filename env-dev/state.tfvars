9e38934ed4a8a21060341acb593887d41fdc18f0
GOUTHAM <goutamreddy817@gmail.com>
2025-02-22 18:36:12 +0530

    terraform

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-817"
+key    = "roboshop/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,4 @@
+variable "component_name" {}
+variable "env" {}
+variable "instance_type" {}
+variable "app_port" {}
